b673f83aa767a90aec741fe898b4ca2523cbd622
Gabriel V Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2019-12-02 21:41:59 +0800

    Added Dashboard components for the Heads & Admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
 
 //Components
-import Navbar from './components/layouts/Navbar';
-import Navbar2 from './components/layouts/Navbar2';
+// import Navbar from './components/layouts/Navbar';
+// import Navbar2 from './components/layouts/Navbar2';
+// import DashboardAdmin from './components/layouts/DashboardAdmin';
+import DashboardHead from './components/layouts/DashboardHead';
 
-import 'materialize-css/dist/css/materialize.min.css';
+
 function App() {
   return (
     <div className="App">
-      <Navbar2 />
+      <DashboardHead />
     </div>
   );
 }
---
+++ client/src/components/layouts/DashboardHead.js
@@ -0,0 +1,180 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9) + 1,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+}));
+
+export default function DashboardHead() {
+    const classes = useStyles();
+    const theme = useTheme();
+
+    //States
+    const [open, setOpen] = useState(true);
+
+    // onclick menu toggle
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar style={{ backgroundColor: "#820101" }}>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, {
+                            [classes.hide]: open,
+                        })}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        SOA Web Portal
+          </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <Drawer
+                variant="permanent"
+                className={clsx(classes.drawer, {
+                    [classes.drawerOpen]: open,
+                    [classes.drawerClose]: !open,
+                })}
+                classes={{
+                    paper: clsx({
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    }),
+                }}
+                open={open}
+            >
+                {/* SideNav Drawer Components */}
+                <div className={classes.toolbar}>
+
+                    <Typography>SOA Head - </Typography>
+
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <MenuIcon /> : <MenuIcon />}
+                    </IconButton>
+                </div>
+
+                <Divider />
+                {/* Item List 1 */}
+                <List>
+
+                    <ListItem button>
+                        <ListItemIcon><InboxIcon /></ListItemIcon>
+                        <ListItemText>List 1</ListItemText>
+                    </ListItem>
+
+                </List>
+
+                <Divider />
+
+                {/* Item List 2 */}
+                <List>
+                    <ListItem button>
+                        <ListItemIcon><MailIcon /></ListItemIcon>
+                        <ListItemText>List 2</ListItemText>
+                    </ListItem>
+                </List>
+            </Drawer>
+
+            {/* for the main content */}
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+
+            </main>
+        </div>
+    );
+}

dc703e2fd0932d12b3146cd0a1e399f7f456576c
Gabriel V Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2019-12-24 13:27:02 +0800

    Added authentication Reducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,35 @@
+import {
+    SET_CURRENT_USER,
+    USER_LOADING
+} from "../actions/types";
+import { stat } from "fs";
+
+const isEmpty = require("is-empty");
+
+//InitialState
+const initialState = {
+    isAuthenticated: false,
+    user: {},
+    loading: false
+};
+
+//Reducers
+export default function (state = initialState, action) {
+    switch (action.type) {
+        //Authentication Reducer
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: !isEmpty(action.payload),
+                user: action.payload
+            };
+        case USER_LOADING:
+            return {
+                ...state,
+                loading: true
+            };
+        default:
+            return state;
+
+    }
+}

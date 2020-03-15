81488d0529fa512ac9785a4c12e993a8cc086731
Gavee Agoncillo <gaveeagoncillo@pop-os.localdomain>
2020-03-15 16:26:57 +0800

    EDited Opening messages for the student side of the portal

--- client/src/components/st/StudentIndex.js
+++ client/src/components/st/StudentIndex.js
@@ -5,7 +5,17 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container'
+import Container from '@material-ui/core/Container';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+
+import Button from '@material-ui/core/Button';
 
 // Material UI styles 
 const styles = theme => ({
@@ -14,8 +24,23 @@ const styles = theme => ({
         paddingTop: theme.spacing(2),
         paddingBottom: theme.spacing(2),
     },
+    list: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+    button: {
+        margin: theme.spacing.unit,
+      },
+    input: {
+        display: 'none',
+      },
     
-})
+});
+
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+  }
 
 class StudentIndex extends Component {
     constructor(props) {
@@ -36,6 +61,55 @@ class StudentIndex extends Component {
                     <Typography component="p">
                     In this Student Web Portal, You can :
                     </Typography>
+
+                <div className={classes.root}>
+                    <List component="nav">
+                        {/* List 1  */}
+                        <ListItem button>
+                            <ListItemIcon>
+                                <InboxIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="File Reports for Lost and Found Items" />
+                            </ListItem>
+
+                            {/* L ist 2  */}
+                        <ListItem button>
+                            <ListItemIcon>
+                                <DraftsIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="View BatStateU Organizational Reports" />
+                            </ListItem>
+                    
+
+                        <ListItemLink href="#simple-list">
+                        <ListItemIcon>
+                                <DraftsIcon />
+                            </ListItemIcon>
+                        <ListItemText primary="View Calendar of Events" />
+                        </ListItemLink>
+
+                        <ListItemLink href="#simple-list">
+                        <ListItemIcon>
+                                <DraftsIcon />
+                            </ListItemIcon>
+                        <ListItemText primary="Join to activities led by Student Organizations" />
+                        </ListItemLink>
+
+                        <ListItemLink href="#simple-list">
+                        <ListItemIcon>
+                                <DraftsIcon />
+                            </ListItemIcon>
+                        <ListItemText primary="Download Files you need for getting the service of the university" />
+                        </ListItemLink>
+                    </List>
+                </div>
+                
+                <center>
+                <Button variant="outlined" color="secondary" className={classes.button}>
+                    Secondary
+                </Button>
+                </center>
+
                 </Paper>
                 </Container>
             </div>

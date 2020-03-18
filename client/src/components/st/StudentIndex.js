1ef1e30c7ef313c09a6df756ebf036257c401fb0
Gavee Agoncillo <gaveeagoncillo@pop-os.localdomain>
2020-03-18 14:57:26 +0800

    Remove components that doesn't use

--- client/src/components/st/StudentIndex.js
+++ client/src/components/st/StudentIndex.js
@@ -11,10 +11,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
 import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-
 import Button from '@material-ui/core/Button';
 
 import ViewHeadlineIcon from '@material-ui/icons/ViewHeadline';
@@ -48,9 +45,6 @@ function ListItemLink(props) {
   }
 
 class StudentIndex extends Component {
-    constructor(props) {
-        super(props)
-    }
 
     render() {
         const { classes } = this.props;

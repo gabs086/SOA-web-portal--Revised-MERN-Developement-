b78d51eaa76cee018f4e3ce3fedc1472f42bd9b9
Gavee Agoncillo <gaveeagoncillo@pop-os.localdomain>
2020-03-18 13:31:14 +0800

    Added laf.js for Lost and found HTTP request

---
+++ routes/api/laf.js
@@ -0,0 +1,13 @@
+// This api will be responsible for all the HTTP request in the lost and found Form 
+
+// The form will request the following.
+// - Name of the Student
+// - Their SR-Code
+// - What year that student is
+// - What campus 
+// - what department
+// - their course 
+// - details of the lost item 
+// - Contact Email or Mobile phone number)
+// -  Status of the report (default = Unfound/Unclaimed)
+

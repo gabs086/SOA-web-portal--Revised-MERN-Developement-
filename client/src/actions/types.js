8ea9e2524d1a260c0b543bb51fcd3bf42269aaee
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-03-25 17:26:11 +0800

    Added USER_LOADED in types of reducers

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,5 @@
 //Login Reducers Type
 export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = 'USER_LOADED';
 export const SET_CURRENT_USER = "SET_CURRENT_USER";

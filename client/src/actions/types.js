9a2527b742eb7def9e823fb09fffe7d797b74ca5
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-03-27 15:30:50 +0800

    Added reducer types for lost and found reports

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,7 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+//LOST and Found Reducers
+export const ADD_LOST_REPORT = 'ADD_LOST_REPORT';
+export const GET_LOST_REPORTS = 'GET_LOST_REPORT';

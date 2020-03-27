d0706387011477b9731f30b0c847dc399bfb2e87
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-03-27 15:41:22 +0800

    Added reducer for adding a lost item report

---
+++ client/src/reducers/lostAndFoundReducer.js
@@ -0,0 +1,23 @@
+import {
+    ADD_LOST_REPORT
+} from '../actions/types';
+
+// Initial State 
+const initialState = {
+    lost: false,
+    found: false,
+    reports: [],
+    loading: false
+}
+
+export default function (state = initialState, action){
+    switch(action.type){
+        case ADD_LOST_REPORT:
+            return {
+                ...state,
+                lost: true,
+                reports: [action.payload, ...state.reports]
+                
+            };
+    }
+}

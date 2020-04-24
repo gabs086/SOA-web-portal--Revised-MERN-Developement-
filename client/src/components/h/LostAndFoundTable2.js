874d6e17f954ee5f05fbcf19b1a5f125c7dd7f65
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-04-24 16:09:54 +0800

    Added useEffect method

--- client/src/components/h/LostAndFoundTable2.js
+++ client/src/components/h/LostAndFoundTable2.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles, makeStyles, useTheme,} from '@material-ui/core/styles';

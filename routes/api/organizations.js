f009f65111022044e4da24d47f0c3da7d65708cf
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-05-13 20:55:34 +0800

    Added new file for the organizations api endpoints

---
+++ routes/api/organizations.js
@@ -0,0 +1,12 @@
+// This file will contain the registration of organization and their  account 
+
+// For registration of Organizations, the data's to be needed will be
+// 1. Campus of the organization 
+// 2. Organizations Department 
+// 3. Registering Organization Name
+// 4. Registering Organization President Name
+// 5. Registering Organization Adviser  Name
+// 6. Quantity of members 
+// 7. Quantity of Officers 
+// 8. Org Description 
+// 9. Registering date 

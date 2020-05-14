a01449f6375b6e2ea60a627322f4ce8b207332b8
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-05-14 15:40:54 +0800

    added enpoint for getting all the data

--- routes/api/orgdesc.js
+++ routes/api/orgdesc.js
@@ -16,6 +16,19 @@ const router = express.Router();
 
 const OrgDesc = require('../../models/orgdesc.model');
 
+//@route GET /api/orgdesc/getorgdesc
+//@desc Get all the registered organizations data
+//@access Admin and Head only
+router.get('/getorgdesc', async (req, res) => {
+	const org = await OrgDesc.findAll();
+
+	try{
+		if(org) res.json(org);
+	}
+	catch(err){
+		res.status(500).json(err);
+	}
+});
 
 
 module.exports = router;

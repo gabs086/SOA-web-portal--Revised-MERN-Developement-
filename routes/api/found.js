80637b0b92b25a428c7fad757b0e1b98aabb36d8
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-05-16 14:29:18 +0800

    Created file that will contain the api for lost and found

---
+++ routes/api/found.js
@@ -0,0 +1,8 @@
+// This File will contain the inputs for getting the reports of found inputs 
+
+// The data will have the ff
+// 1.findername
+// 2.founditem
+// 3.campus
+// 4.date the item has been found
+// 5.created_at

cd5d52713fd8c012071ee1ff898ef35866d711cf
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-05-19 23:21:58 +0800

    Added date-io/date-fns package, added material-ui/pickers package and setup the form for Found Item submittion

--- client/src/components/h/AddFound.js
+++ client/src/components/h/AddFound.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import { connect } from "react-redux";
+import moment from 'moment';
 
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker, } from '@material-ui/pickers';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
@@ -12,6 +17,11 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
 
 import DashBoardHead from '../layouts/DashboardHead';
 
@@ -59,9 +69,71 @@ const styles = theme => ({
 
 function AddFound(props){
 
+
     const classes = props;
 
-console.log(props);
+    // State for the campuses and its loading state
+  const [campuses, getCampuses] = useState([]);
+  const [loadingCampuses, setLoadingCampuses] = useState(true);
+ 
+  // Value States 
+  const [finderName ,setFinderName]  = useState('');
+  const [foundItem, setFoundItem] = useState('');
+  const [campus, setCampus] = useState('');
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+// Value functions : 
+
+// For Findername State 
+const handleFinderName = e => {
+        e.preventDefault();
+    setFinderName(e.target.value);
+};
+
+// For FoundItem State
+const handleFoundItem = e => {
+        e.preventDefault();
+    setFoundItem(e.target.value);
+}
+
+// For campus State 
+const handleCampus = e => {
+    e.preventDefault();
+
+    setCampus(e.target.value);
+};
+
+// For selectedDate state 
+const handleDateChange = date => {
+    setSelectedDate(date);
+};
+
+
+const handleSubmit = e => {
+
+    e.preventDefault();
+
+    const dateData = moment(selectedDate).format('YYYY-MM-DD, h:mm:ss a');
+
+    const newFoundRecord = {
+        finderName,
+        foundItem,
+        campus,
+        dateData
+    };
+
+    console.log(newFoundRecord);
+};
+
+
+    useEffect( _ => {
+        (async _ => {
+            const res = await axios.get('/api/campuses');
+            getCampuses(res.data);
+            setLoadingCampuses(false);
+        })();
+    }, []);
+
   return (
      <DashBoardHead>
 
@@ -95,7 +167,7 @@ console.log(props);
          <div className={classes.root}>    
             
                 <main className={classes.layout}>
-                    <Paper className={classes.paper}>
+                    <Paper className={classes.paper} style={{ paddingTop: '10px', paddingBottom:'30px'}}>
 
                         <Typography variant="h4" align="center">
                             Lost Item Report Form
@@ -107,13 +179,15 @@ console.log(props);
                             Input the details needed.
                         </Typography>
 
-                        <form noValidate>
+                        <form noValidate onSubmit={handleSubmit}>
 
                         <Grid container spacing={3}>
 
                             {/* Ful Name Text Fiel  */}
-                        <Grid item xs={12} md={12}>
+                        <Grid item xs={12}>
                                 <TextField
+                                value={finderName}
+                                onChange={handleFinderName}
                                     id="findername"
                                     name="findername"
                                     label="Founder Name"
@@ -123,8 +197,10 @@ console.log(props);
                            
                                 </Grid>
 
-                          <Grid item xs={12} md={12}>
+                          <Grid item xs={12}>
                                 <TextField
+                                value={foundItem}
+                                onChange={handleFoundItem}
                                     id="founditem"
                                     name="founditem"
                                     label="Found Item"
@@ -132,8 +208,72 @@ console.log(props);
                                     autoComplete="founditem"
                                 />
                            
-                                </Grid>
+                            </Grid>
+
+                       { /* Campuses Selection */ }
+                        <Grid item xs={12}>
+                            {/* Campuses TextField */}
+                            <FormControl fullWidth className={classes.formControl}>
+
+                            <InputLabel htmlFor="campus-simple">Campus</InputLabel>
+
+                                <Select
+                                    value={campus}
+                                    onChange={handleCampus}
+                                    
+                                    inputProps={{
+                                        name:'campus',
+                                        id: 'campus-simple'
+                                    }}
+                                >
+
+                                {
+                                    loadingCampuses ?
+                                        <MenuItem></MenuItem>
+                                    :
+                                    campuses.map((campus, id) => {
+                                        return( <MenuItem key={id} value={campus.campusname}>{campus.campusname} </MenuItem>)
+                                    })
+                                }
+
+
+                                </Select>
+
+                            </FormControl>
+                        </Grid>
                                
+                       { /* Date Selection */ }
+                        <Grid item xs={12}>
+                         <FormControl fullWidth className={classes.formControl}>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                    <KeyboardDatePicker
+                                      disableToolbar
+                                      variant="inline"
+                                      format="MM/dd/yyyy"
+                                      margin="normal"
+                                      id="date-picker-inline"
+                                      label="Date picker inline"
+                                      value={selectedDate}
+                                      onChange={handleDateChange}
+                                      KeyboardButtonProps={{
+                                        'aria-label': 'change date',
+                                      }}
+                                    />
+                            </MuiPickersUtilsProvider>
+                          </FormControl>
+                        </Grid>
+
+
+                         <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Add
+                            </Button>
+
+
 
                         </Grid>
 

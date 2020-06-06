6351b8a986c41101d648e529cb7c96e9707ee0a2
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-06-06 20:40:16 +0800

    Change the structure of the OrgIndex Component to react hooks

--- client/src/components/org/OrgIndex.js
+++ client/src/components/org/OrgIndex.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
 import axios from 'axios';
 
@@ -7,39 +6,21 @@ import Container from '@material-ui/core/Container';
 
 import Navbar2 from "../layouts/Navbar2";
 
-export class OrgIndex extends Component {
-	constructor(props){
-		super(props)
+function OrgIndex(props) {
 
-		this.state = {
-			orgOnline: ''
-		}
+	const [orgOnline, setOrgOnline] = useState('');
 
-	}
-
-	componentDidMount(){
-
-		const { auth } = this.props;
+	useEffect(_ => {
+		const { auth } = props;
 
 		axios.get('/api/org/getorgaccnts')
 		.then(res => {
-			
-			// console.log(res.data)
-
 			res.data.filter(org => auth.user.username === org.username)
-			.map(org => this.setState({ orgOnline: org.orgname }));
-
+			.map(org => setOrgOnline(org.orgname))
 		})
-		.catch(err => err);
-	}
+		.catch(err => err)
 
-    render() {
-
-    	const { orgOnline } = this.state;
-
-    	// console.log(auth)
-
-    	console.log(orgOnline);
+	},[]);
 
         return (
             <div>
@@ -51,12 +32,7 @@ export class OrgIndex extends Component {
 
             </div>
         )
-    }
 }
-
-OrgIndex.propTypes = {
-	auth: PropTypes.object.isRequired
-};
 
 const mapStateToProps = state => ({
 	auth:state.auth

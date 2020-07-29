25176813eddfa9bc64d34daab7567b1bf2ce40b6
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-07-29 21:01:03 +0800

    Created model for announcements

---
+++ models/announcements.model.js
@@ -0,0 +1,64 @@
+/* 
+This model schema is for the announcement table
+This model will consist of
+
+-	 - id
+	 - title
+	 - date
+	 - dateDate
+	 - dateTime
+	 - bgColor
+	 - venue
+	 - description
+	 - poster
+	 - setBy
+	 - created_at
+
+*/
+
+const Sequelize = require('sequelize');
+const db = require('../config/db');
+
+
+module.exports = db.sequelize.define(
+	'announcements', {
+		id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		title: {
+			type: Sequelize.STRING
+		},
+		date: {
+			type: Sequelize.DATE,
+		}
+		dateDate: {
+			type: Sequelize.STRING
+		},
+		dateTime: {
+			type: Sequelize.STRING
+		},
+		bgColor: {
+			type: Sequelize.STRING
+		},
+		venue: {
+			type: Sequelize.STRING
+		},
+		description: {
+			type: Sequelize.STRING
+		},
+		poster: {
+			type: Sequelize.STRING
+		},
+		setBy: {
+			type: Sequelize.STRING
+		},
+		created_at: {
+			type: Sequelize.DATE,
+        	defaultValue: Sequelize.NOW
+		},
+	}, {
+		timestamps: false	
+	}
+	)

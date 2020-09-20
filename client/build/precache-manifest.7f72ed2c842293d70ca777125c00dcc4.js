ee76ed880d995325fa5bf6f1700aa8c526fd2d1a
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-09-20 15:35:50 +0800

    Commented the build in the .gitignore of client side

---
+++ client/build/precache-manifest.7f72ed2c842293d70ca777125c00dcc4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3698650c585118da974d9054ac7787d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b64ef1f5afb1dc2233fc",
+    "url": "/static/css/2.871b9f6f.chunk.css"
+  },
+  {
+    "revision": "60fdd472eba454e709e7",
+    "url": "/static/css/main.e763f7c2.chunk.css"
+  },
+  {
+    "revision": "b64ef1f5afb1dc2233fc",
+    "url": "/static/js/2.1629b01b.chunk.js"
+  },
+  {
+    "revision": "b72ded70c7cff4dff1a02cb40bc63544",
+    "url": "/static/js/2.1629b01b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fdd472eba454e709e7",
+    "url": "/static/js/main.7fb38ccc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "689b151579e1613b33f6d9c5a080f92c",
+    "url": "/static/media/640x640_12988565.689b1515.png"
+  },
+  {
+    "revision": "890b8cbcf6160e2c27ad2c8c8a66ee55",
+    "url": "/static/media/download.890b8cbc.png"
+  },
+  {
+    "revision": "8d2c65beab42ba0533b2c1215659bae3",
+    "url": "/static/media/files-sharing.8d2c65be.jpg"
+  },
+  {
+    "revision": "24acc2dd07c28c352f4269bb05bbe0c1",
+    "url": "/static/media/images.24acc2dd.png"
+  },
+  {
+    "revision": "4ccd5ec6d3922c51275905445e2e191c",
+    "url": "/static/media/indexhead.4ccd5ec6.jpg"
+  },
+  {
+    "revision": "f0f35d57b508db34a6f528cf092ff863",
+    "url": "/static/media/indexnewpic.f0f35d57.png"
+  },
+  {
+    "revision": "400b10e10cc429f1beb876141646a932",
+    "url": "/static/media/reports.400b10e1.png"
+  },
+  {
+    "revision": "806d0b8f4d3ad3de1d96d272e0261ac0",
+    "url": "/static/media/request.806d0b8f.png"
+  }
+]);
